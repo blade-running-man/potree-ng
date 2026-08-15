b8dfd008261c89ff9f8622b4f7260b641c9bb1d7
Oleksandr S <oleksandr.slukovskyi@gmail.com>
2026-08-15 11:09:50 +0200

    refactor(loader): EptLazBatcher awaits runWorker

--- src/loader/ept/LaszipLoader.js
+++ src/loader/ept/LaszipLoader.js
@@ -84,80 +84,64 @@ export class CopcLaszipLoader {
 export class EptLazBatcher {
 	constructor(node) { this.node = node; }
 
-	push(las) {
-		const { isFullFile, compressed, header, eb, pointCount, nodemin } = las
+	async push(las) {
+		const { isFullFile, compressed, header, eb, pointCount, nodemin } = las;
 
-		let workerPath = Potree.scriptPath +
-			'/workers/EptLaszipDecoderWorker.js';
-		let worker = Potree.workerPool.getWorker(workerPath);
+		let workerPath = Potree.scriptPath + '/workers/EptLaszipDecoderWorker.js';
 		const pointAttributes = this.node.owner.pointAttributes;
+		let message = { isFullFile, compressed, header, eb, pointCount, nodemin };
 
-		worker.onmessage = (e) => {
+		try {
+			let data = await Potree.workerPool.runWorker(workerPath, message, [compressed]);
+
 			let g = new THREE.BufferGeometry();
 
-			let positions = new Float32Array(e.data.position);
-			let colors = new Uint8Array(e.data.color);
-			
-			let intensities = new Float32Array(e.data.intensity);
-			let classifications = new Uint8Array(e.data.classification);
-			let returnNumbers = new Uint8Array(e.data.returnNumber);
-			let numberOfReturns = new Uint8Array(e.data.numberOfReturns);
-			let pointSourceIds = new Uint16Array(e.data.pointSourceId);
-			let indices = new Uint8Array(e.data.indices);
-			let gpsTime = new Float32Array(e.data.gpsTime);
+			let positions = new Float32Array(data.position);
+			let colors = new Uint8Array(data.color);
+			let intensities = new Float32Array(data.intensity);
+			let classifications = new Uint8Array(data.classification);
+			let returnNumbers = new Uint8Array(data.returnNumber);
+			let numberOfReturns = new Uint8Array(data.numberOfReturns);
+			let pointSourceIds = new Uint16Array(data.pointSourceId);
+			let indices = new Uint8Array(data.indices);
+			let gpsTime = new Float32Array(data.gpsTime);
 
-			g.setAttribute('position',
-					new THREE.BufferAttribute(positions, 3));
-			g.setAttribute('rgba',
-					new THREE.BufferAttribute(colors, 4, true));
-			g.setAttribute('intensity',
-					new THREE.BufferAttribute(intensities, 1));
-			g.setAttribute('classification',
-					new THREE.BufferAttribute(classifications, 1));
-			g.setAttribute('return number',
-					new THREE.BufferAttribute(returnNumbers, 1));
-			g.setAttribute('number of returns',
-					new THREE.BufferAttribute(numberOfReturns, 1));
-			g.setAttribute('source id',
-					new THREE.BufferAttribute(pointSourceIds, 1));
-			g.setAttribute('indices',
-					new THREE.BufferAttribute(indices, 4));
-			g.setAttribute('gps-time',
-					new THREE.BufferAttribute(gpsTime, 1));
-			this.node.gpsTime = e.data.gpsMeta;
+			g.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+			g.setAttribute('rgba', new THREE.BufferAttribute(colors, 4, true));
+			g.setAttribute('intensity', new THREE.BufferAttribute(intensities, 1));
+			g.setAttribute('classification', new THREE.BufferAttribute(classifications, 1));
+			g.setAttribute('return number', new THREE.BufferAttribute(returnNumbers, 1));
+			g.setAttribute('number of returns', new THREE.BufferAttribute(numberOfReturns, 1));
+			g.setAttribute('source id', new THREE.BufferAttribute(pointSourceIds, 1));
+			g.setAttribute('indices', new THREE.BufferAttribute(indices, 4));
+			g.setAttribute('gps-time', new THREE.BufferAttribute(gpsTime, 1));
+			this.node.gpsTime = data.gpsMeta;
 
 			g.attributes.indices.normalized = true;
 
-			for (const key in e.data.ranges) {
-				const range = e.data.ranges[key];
+			for (const key in data.ranges) {
+				const range = data.ranges[key];
 				const attribute = pointAttributes.attributes.find(a => a.name === key);
 				if (attribute) {
 					attribute.range[0] = Math.min(attribute.range[0], range[0]);
 					attribute.range[1] = Math.max(attribute.range[1], range[1]);
-
-					// May not be right, but we need something here or we crash.
 					if (!attribute.initialRange) {
-						attribute.initialRange = attribute.range
+						attribute.initialRange = attribute.range;
 					}
 				}
 			}
 
 			let tightBoundingBox = new THREE.Box3(
-				new THREE.Vector3().fromArray(e.data.tightBoundingBox.min),
-				new THREE.Vector3().fromArray(e.data.tightBoundingBox.max)
+				new THREE.Vector3().fromArray(data.tightBoundingBox.min),
+				new THREE.Vector3().fromArray(data.tightBoundingBox.max)
 			);
 
-			this.node.doneLoading(
-				g,
-				tightBoundingBox,
-				pointCount,
-				new THREE.Vector3(...e.data.mean));
-
-			Potree.workerPool.returnWorker(workerPath, worker);
-		};
-
-		let message = { isFullFile, compressed, header, eb, pointCount, nodemin };
-
-		worker.postMessage(message, [message.compressed]);
+			this.node.doneLoading(g, tightBoundingBox, pointCount, new THREE.Vector3(...data.mean));
+		} catch (err) {
+			console.error(`EptLaszipDecoderWorker failed for node ${this.node.name}:`, err);
+			this.node.loading = false;
+		} finally {
+			Potree.numNodesLoading--;
+		}
 	};
 };
